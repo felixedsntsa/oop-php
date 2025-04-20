9e716551c930e34d9def9cfd973322f1cca1e05c
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-20 12:51:30 +0700

    belajar abstract dan interface

---
+++ abstract-class.php
@@ -0,0 +1,152 @@
+<?php 
+
+// class produk
+abstract class Produk {
+    private $judul,
+    $penulis,
+    $penerbit,
+    $harga,
+    $diskon = 0;
+    
+    // construct class produk
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    // method set judul
+    public function setJudul($judul) {
+        // validasi judul harus string
+        if(!is_string($judul)) {
+            throw new Exception(("Judul harus string!"));
+        }
+        $this->judul = $judul;
+    }
+
+    // method get judul
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    // method set penulis
+    public function setPenulis($penulis) {
+        // validasi penulisa harus string
+        if(!is_string($penulis)) {
+            throw new Exception(("Penulis harus string!"));
+        }
+        $this->penulis = $penulis;
+    }
+
+    // method get penulis
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    // method set penerbit
+    public function setPenerbit($penerbit) {
+        // validasi penulisa harus string
+        if(!is_string($penerbit)) {
+            throw new Exception(("Penerbit harus string!"));
+        }
+        $this->penerbit = $penerbit;
+    }
+
+    // method get penulis
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
+    // method set diskon
+    public function setDiskon($diskon) {
+        $this->diskon = $diskon;
+    }
+
+    // method get diskon
+    public function getDiskon() {
+        return $this->diskon;
+    }
+
+    // mencegah harga diakses langsung dari luar class produk
+    public function getHarga() {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function getLabel() {
+        return "$this->penulis | $this->penerbit";
+    }
+
+    // abstract class
+    abstract public function getInfoProduk();
+
+    // method 
+    public function getInfo() {
+        $str = "{$this->judul} | {$this->getLabel()} | Rp{$this->harga}";
+        return $str;
+    }
+    
+}
+
+// class komik
+class Komik extends Produk {
+    public $jmlHalaman;
+    // contruct class komik
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+        // ambil constructor dari class produk / parent
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    // menjalankan method abstract
+    public function getInfoProduk() {
+        $str = "Komik : " . $this->getInfo() . " | {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+// class game
+class Game extends Produk {
+    public $waktuMain;
+    // construct class game
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
+        // ambil constructor dari class produk / parent
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+
+
+    // menjalankan method abstract
+    public function getInfoProduk() {
+        $str = "Game : " . $this->getInfo() . " | {$this->waktuMain} jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk {
+    public $daftarProduk = array();
+
+    public function tambahProduk(Produk $produk) {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak() {
+        $str = "Daftar Produk : <br>";
+
+        foreach($this->daftarProduk as $pr) {
+            $str .= " - {$pr->getInfoProduk()} <br>";
+        }
+
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Honkai Star Rail", "HoYoverse", "HoYoverse", 0, 100);
+$produk3 = new Komik("kimi No Nawa", "Makoto Shinkai", "Kadokawa", 90000, 300);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+$cetakProduk->tambahProduk($produk3);
+echo $cetakProduk->cetak();
